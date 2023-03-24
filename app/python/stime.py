ddb7c71b383044f15b011ae14ece702b5e9c7027
Leiger <max.allemeersch@gmail.com>
2023-03-24 15:32:18 +0100

    update interface and docker

--- app/python/stime.py
+++ app/python/stime.py
@@ -1,10 +1,16 @@
+__author__ = "ALLEMEERSCH Maxime, CAVRENNE Louis, PANS Benjamin, PASTORI Lucas"
+
 import mysql.connector
 from datetime import date
+import os
 
-### GESTION BASE DE DONNEES ###
+
+##################################
+###   GESTION BASE DE DONNEES  ###
+##################################
 
 connection = mysql.connector.connect(
-    user='root', password='root', host='mysql', port="3306", database='dbstime')
+    user='root', password='root', host='localhost', port="3306", database='dbstime')
 print("DB connected")
 
 cursor = connection.cursor()
@@ -12,8 +18,138 @@ cursor.execute('Select * FROM user')
 users = cursor.fetchall()
 connection.close()
 
+##################################
+###          TERMINAL          ###
+##################################
+
+# Nettoyer le terminal
+def clear_terminal():
+    if os.name == "posix":
+        os.system("clear")
+    else:
+        os.system("cls")
+
+# Quitter le terminal
+def leave():
+    clear_terminal()
+    print("Leave the Stime platform...")
+    exit()
+
+##################################
+###          LIBRARY           ###
+##################################
+
+
+def show_library():
+    clear_terminal()
+    print('\033[1;34m[YOUR LIBRARY]\033[0m \n')
+
+    ## produire le SQL query en dessous
+    # Récupérer les jeux de l'utilisateur dans la base de données
+    games = [('Game Title 1',), ('Game Title 2',), ('Game Title EXTRA LONG 3',), ('Game Title 4',)] #A SUPPRIMER 
+    
+    if len(games) == 0:
+        print("You don't have any games yet. You can visit the shop for buy a new game")
+    else:
+       # print("You have" + numberGames + " games \n")
+
+        # Longueur maximale des titres de jeu
+        game_title_size  = 0
+        for game in games:
+            if len(game[0]) > game_title_size :
+                game_title_size = len(game[0])
+
+        # Espace devant le titre
+        space_before_title = 2
+
+        # Largeur totale du tableau
+        total_width = (space_before_title * 2) + game_title_size
+
+        # Ligne supérieure du tableau
+        print('|' + '-' * (total_width + 2) + '|')
+
+        # Afficher les titres de jeu dans des cellules de tableau
+        for game in games:
+            game_title = game[0]
+            padding_width = total_width - len(game_title) - (space_before_title * 2)
+            padding = ' ' * padding_width
+            print(f'|{" " * space_before_title}{game_title}{padding}{" " * space_before_title}  |')
+            # Ligne inférieur du tableau
+            print('|' + '-' * (total_width + 2) + '|')
+
+##################################
+###         GAME SHOP          ###
+##################################
+
+def show_game_shop():
+    clear_terminal()
+    print('\033[1;34m[STORE]\033[0m \n')
+
+    ## produire le SQL query en dessous
+
+##################################
+###         ITEM SHOP          ###
+##################################
+
+def show_item_shop():
+    clear_terminal()
+    print('\033[1;34m[ITEM MARKET]\033[0m \n')
+
+    ## produire le SQL query en dessous
+
+##################################
+###         INVENTORY          ###
+##################################
+
+def my_item():
+    clear_terminal()
+    print('\033[1;34m[YOUR ITEM]\033[0m \n')
+
+    ## produire le SQL query en dessous
+
+
+##################################
+###      MENU PLATFORME        ###
+##################################
+
+def show_menu():
+    clear_terminal()
+    username = "max"
+
+    print("Welcome \033[1;34m" + username + "\033[0m ! \n")
+    print('what do you wish to do ? Enter the number you wish for :')
+    print('1.  See my games')
+    print('2.  Buy a game')
+    print('3.  Buy / sell an item')
+    print('4.  See my items' )
+    print('5.  Leave')
+    print('')
+
+    choice2 = int(input())
+
+    match choice2:
+        case 1:
+            show_library()
+        case 2:
+            show_game_shop()
+        case 3:
+            show_item_shop()
+        case 4:
+            my_item()
+        case 5:
+            leave()
+
+
+##################################
+###       LOGIN/REGISTRE       ###
+##################################
+
+
 ## fonction d'inscription
 def register():
+    clear_terminal()
+    print('\033[1;34m[REGISTER]\033[0m \n')
+    print('Please enter the following information:')
     print('Choose a username')
     username = str(input())
     print('What is your password')
@@ -28,61 +164,51 @@ def register():
     ## retravailler, pose soucis
     ##
     ##
-    sql = "INSERT INTO user (Username, Password, Firstname, Surname, Email, Birthdate, DateInscription) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-    now = date.today()
-    val = (username, password, firstname, name, email, now, now)
-    cursor.execute(sql, val)
+    #sql = "INSERT INTO user (Username, Password, Firstname, Surname, Email, Birthdate, DateInscription) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    #now = date.today()
+    #val = (username, password, firstname, name, email, now, now)
+    #cursor.execute(sql, val)
+    # Une fois l'inscription effecuté
+    login_register_menu()
 
+
+## fonction login
 def login():
+    clear_terminal()
+    print('\033[1;34m[LOGIN]\033[0m \n')
+    print('Please enter the following information:')
     print('Enter your username')
     username = str(input())
     print('Enter your password')
     password = str(input())
+    show_menu()
 
     ## produire le SQL query en dessous
-
-def show_library():
-    print('library page')
-
-    ## produire le SQL query en dessous
-
-def show_game_shop():
-    print('shop page')
-
-    ## produire le SQL query en dessous
-
-def show_item_shop():
-    print('Item shop page')
-
-    ## produire le SQL query en dessous
-
-def my_item():
-    print("Item page'")
+    ##IF login OK -> show_menu()
 
 
-### GESTION INTERFACE ###
+## fonction menu login/register
+def login_register_menu():
+    clear_terminal()
+    print("\033[1;34m********************************************")
+    print("*                                          *")
+    print("*       Welcome to the Stime platform!     *")
+    print("*                                          *")
+    print("******************************************** \033[0m \n")
+    print('What do you wish to do ? Enter the number you wish for :')
+    print('1.  Register')
+    print('2.  Login')
+    print('3.  Leave')
 
-print('what do you wish to do ? Enter the number you wish for :')
-print('1.  Register')
-print('2.  Login')
-print('3.  See my games')
-print('4.  Buy a game')
-print('5.  Buy / sell an item')
-print('6.  See my items' )
-print('')
+    choice1 = int(input())
 
-choice1 = int(input())
+    match choice1:
+        case 1:
+            register()
+        case 2:
+            login()
+        case 3:
+            leave()
 
-match choice1:
-    case 1:
-        register()
-    case 2:
-        login()
-    case 3:
-        show_library()
-    case 4:
-        show_game_shop()
-    case 5:
-        show_item_shop()
-    case 6:
-        my_item()
+# Main 
+login_register_menu()    
