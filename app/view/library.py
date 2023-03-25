755d22f5197dcd746c53ef4fcadb43a5bb5092ce
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-03-25 21:54:04 +0100

    add game library view

--- app/view/library.py
+++ app/view/library.py
@@ -1,39 +1,42 @@
-from app.view.console_utils.io import clear_console
+from app.view.console_utils.colors import BLUE
+from app.view.console_utils.io import clear_console, color_print
 
 
-def show_library():
+def show_library(games):
+    """
+    Display games of the user in a beautiful array.
+    :param games: the list of games to show
+    """
+
+    # games = [('Game Title 1',), ('Game Title 2',), ('Game Title EXTRA LONG 3',), ('Game Title 4',)]  # A SUPPRIMER
+
     clear_console()
-    print('\033[1;34m[YOUR LIBRARY]\033[0m \n')
-
-    ## produire le SQL query en dessous
-    # Récupérer les jeux de l'utilisateur dans la base de données
-    games = [('Game Title 1',), ('Game Title 2',), ('Game Title EXTRA LONG 3',), ('Game Title 4',)]  # A SUPPRIMER
+    color_print("[YOUR LIBRARY]", BLUE)
 
     if len(games) == 0:
-        print("You don't have any games yet. You can visit the shop for buy a new game")
+        print("You don't have any games yet. You can visit the shop to buy new games.")
     else:
-        # print("You have" + numberGames + " games \n")
+        print(f"You have {len(games)} game(s).\n")
 
-        # Longueur maximale des titres de jeu
+        # Max length for game titles
         game_title_size = 0
         for game in games:
             if len(game[0]) > game_title_size:
                 game_title_size = len(game[0])
 
-        # Espace devant le titre
+        # Spaces before title
         space_before_title = 2
 
-        # Largeur totale du tableau
+        # Total width
         total_width = (space_before_title * 2) + game_title_size
 
-        # Ligne supérieure du tableau
+        # Top of the array
         print('|' + '-' * (total_width + 2) + '|')
 
-        # Afficher les titres de jeu dans des cellules de tableau
         for game in games:
             game_title = game[0]
             padding_width = total_width - len(game_title) - (space_before_title * 2)
             padding = ' ' * padding_width
             print(f'|{" " * space_before_title}{game_title}{padding}{" " * space_before_title}  |')
-            # Ligne inférieur du tableau
+            # Lower lines for the array
             print('|' + '-' * (total_width + 2) + '|')
