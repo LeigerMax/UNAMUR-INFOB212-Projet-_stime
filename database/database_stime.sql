f1e54e0bcedeb12ed87fd9e6821871861b3cf41f
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-03-26 13:03:00 +0200

    change base sql script

--- database/database_stime.sql
+++ database/database_stime.sql
@@ -3,10 +3,15 @@ use dbstime;
 
 CREATE TABLE user(
     userid int not null AUTO_INCREMENT,
+    Username varchar(100) NOT NULL,
+    Password varchar(256) NOT NULL,
     FirstName varchar(100) NOT NULL,
-    Surname varchar(100) NOT NULL,
+    LastName varchar(100) NOT NULL,
+    BirthDate DATE NOT NULL,
+    DateInscription DATETIME NOT NULL,
     PRIMARY KEY (userid)
 );
 
-INSERT INTO user(FirstName, Surname)
-VALUES("John", "Andersen"), ("Emma", "Smith");  
+INSERT INTO user(Username, Password, FirstName, LastName, BirthDate, DateInscription)
+VALUES("john_a", "password", "John", "Andersen", "2000-01-01", NOW()),
+      ("emma_e", "password", "Emma", "Smith", "1998-02-03", NOW());
