54da416484198bf5d15585d3c9048d4f6b768138
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-03-29 18:19:35 +0200

    update python version check

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from settings import PyVersion
 
 def main():
     # Version check
-    if not (sys.version_info[0] == PyVersion.MAJOR and sys.version_info[1] >= PyVersion.MINOR):
+    if not (sys.version_info[0] >= PyVersion.MAJOR and sys.version_info[1] >= PyVersion.MINOR):
         raise Exception(f"This project requires Python {PyVersion.MAJOR}.{PyVersion.MINOR} or higher")
 
     welcome_menu()
