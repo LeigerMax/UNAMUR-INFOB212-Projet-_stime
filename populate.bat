ef055428751ead3be2d63638066a39dd6e71f35a
Louis Cavrenne <louis.cavrenne@student.unamur.be>
2023-05-18 21:05:19 +0200

    add debug messages for populate scripts

--- populate.bat
+++ populate.bat
@@ -1,1 +1,8 @@
+@echo off
+
 mysql -h localhost --user=root --password=supersecretpassword123 --database=dbstime < %~dp0\database\insert_dml.sql
+
+if %ERRORLEVEL% neq 0
+    echo "Stime database successfully populated !"
+else
+    echo "Something when wrong while populating Stime database :/"
--- populate.sh
+++ populate.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 mysql -h localhost -P 3306 -u root -p'supersecretpassword123' -D dbstime --protocol=tcp < ./database/insert_dml.sql
+
+if [ $? -eq 0 ]; then
+    echo "Stime database successfully populated !"
+else
+    echo "Something when wrong while populating Stime database :/"
+fi
