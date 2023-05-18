00ce79831350264a37b023ac8fa5287d068dcbac
PastoriLucas <lucas.pastori@student.unamur.be>
2023-05-18 22:35:01 +0200

    Update inventory.py, item_shop_details.py, and 13 more files...

--- app/controller/item_shop_platform.py
+++ app/controller/item_shop_platform.py
@@ -1,4 +1,4 @@
-from app.controller.item_shop_details import item_shop_details
+from app.controller.item_shop_details_platform import item_shop_details_platform
 from app.model.objet import Objet
 from app.view.item_shop_platform import item_shop_platform_view
 
@@ -9,8 +9,8 @@ def item_shop_platform(username):
 
     user_choice = item_shop_platform_view(all_objet)
 
-    if user_choice==0:
-        return
+    if user_choice != 0 :
+        item_shop_details_platform(username,user_choice)
     else :
-        item_shop_details(username,user_choice,0)
+        return
 
--- app/controller/shop_panier.py
+++ app/controller/shop_panier.py
@@ -1,3 +1,5 @@
+from app.model.objet import Objet
+from app.model.objet_instance import ObjetInstance
 from app.view.shop_panier import shop_panier_view,shop_panier_delete_elem_view
 from app.controller.shop_panier_bought import shop_panier_bought
 from app.model.panier import Panier
@@ -10,19 +12,30 @@ def shop_panier(username):
     
      #Récupère les ids des jeux dans le panier
      game_in_panier = Panier.get_jeux(Panier(panier_id))
+     objet_in_panier = Panier.get_objet_instances(Panier(panier_id))
      game_ids = []
+     objet_ids = []
      data_game_in_panier = []
+     data_objet_in_panier = []
 
      for id in game_in_panier:
           game_ids.append(id.game_id)
+
+     for id in objet_in_panier:
+          objet_ids.append(id.id)
 
      #Récupère les données des jeux
      for game_id in game_ids:
           jeu = Jeu.select(game_id)
           data_game_in_panier.append((jeu.game_id,jeu.nom, jeu.prix))
 
+     for objet_id in objet_ids:
+          objetI = ObjetInstance.select(objet_id)
+          realObjet = Objet.select(objetI.objet)
+          data_objet_in_panier.append((realObjet.nom, realObjet.description, realObjet.price))
 
-     data_user_choice = shop_panier_view(username,data_game_in_panier)
+
+     data_user_choice = shop_panier_view(username,data_game_in_panier, data_objet_in_panier)
      user_choice,total_price = data_user_choice
      match user_choice:
           case 1:
