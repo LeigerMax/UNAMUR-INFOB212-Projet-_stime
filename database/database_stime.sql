1ffaedbbd5b0fc4ad64346d4f98ed77b264e5b60
Leiger <max.allemeersch@gmail.com>
2023-05-18 21:10:59 +0200

    add reviews

--- database/database_stime.sql
+++ database/database_stime.sql
@@ -383,9 +383,6 @@ INSERT INTO OBJET_INSTANCE (Objet) VALUES (1);
 INSERT INTO OBJET_INSTANCE (Objet) VALUES (2);
 INSERT INTO OBJET_INSTANCE (DateObtention, Possesseur, Objet) VALUES ('2022-06-06', 1 , 1);
 
-INSERT INTO UTILISATEUR_ABONNEMENT (Utilisateur, Abonnement, DateDebut, Duree) VALUES (4,"Basique", "2022-03-01", 30);
-INSERT INTO UTILISATEUR_ABONNEMENT (Utilisateur, Abonnement, DateDebut, Duree) VALUES (5,"Premium", "2022-03-01", 30);
-INSERT INTO UTILISATEUR_ABONNEMENT (Utilisateur, Abonnement, DateDebut, Duree) VALUES (6,"Ultimate", "2022-03-01", 30);
 
 INSERT INTO TRANSACTION (DateMiseEnVente, PrixVente, Revendeur, Objet) VALUES ("2022-05-06", 10, 1, 1);
 
